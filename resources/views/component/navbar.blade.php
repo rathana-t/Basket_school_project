5141e9fa962016eea9bf43481f9102a48d46abeb
Sivcheu <choeng.sivcheu02@gmail.com>
2021-07-15 14:40:32 +0700

    add navbar to home page

---
+++ resources/views/component/navbar.blade.php
@@ -0,0 +1,35 @@
+<nav class="navbar navbar-expand-lg navbar-light ">
+    <div class="container">
+        <a class="navbar-brand" href="#"><img src="/images/logo/PLP.svg" alt="" style="width: 140px"></a>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
+                        data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Category
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="#">KeyBoard</a>
+                        <a class="dropdown-item" href="#">Mouse</a>
+                        <a class="dropdown-item" href="#">Another action</a>
+                    </div>
+                </li>
+                <li class="nav-item search">
+                    <form action="">
+                        <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Search....">
+                    </form>
+                </li>
+            </ul>
+
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item cart-a">
+                    <a class="nav-link" href="#"><img src="/images/logo/opencart.svg" alt=""></a>
+                </li>
+                <li class="nav-item signup-a ml-3">
+                    <a class="nav-link" href="#">SignUp</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
---
+++ resources/views/home/index.blade.php
@@ -0,0 +1,4 @@
+@extends('application')
+
+@section('content')
+@stop
