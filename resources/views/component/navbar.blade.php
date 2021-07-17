d5faa4c55cfe666e8680766a9e2674fd050b0933
Sivcheu <choeng.sivcheu02@gmail.com>
2021-07-17 12:09:23 +0700

    add login to navbar and change some css

--- resources/views/component/navbar.blade.php
+++ resources/views/component/navbar.blade.php
@@ -1,5 +1,5 @@
 <nav class="navbar navbar-expand-lg navbar-light p-0">
-    <div class="container-fluid">
+    <div class="container">
         <a class="navbar-brand logo" href="#"><img src="/images/logo/PLP.svg" alt="" ></a>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav ml-auto">
@@ -24,6 +24,9 @@
             <ul class="navbar-nav ml-auto">
                 <li class="nav-item cart-a mt-1">
                     <a class="nav-link" href="#"><img src="/images/logo/opencart.svg" alt=""></a>
+                </li>
+                <li class="nav-item signup-a ml-3">
+                    <a class="nav-link" href="#">Login</a>
                 </li>
                 <li class="nav-item signup-a ml-3">
                     <a class="nav-link" href="#">SignUp</a>
