6342f5acdc37db787f618e49ce2cc3ae2fad7e65
RathanaThy <rathanathy02@gmail.com>
2021-07-18 08:19:38 +0700

    check session for user and done for function logout

--- routes/web.php
+++ routes/web.php
@@ -33,7 +33,7 @@ Route::get('/blog', [HomeController::class, 'blog']);
 Route::post('/signin', [UserController::class, 'signin'])->name("signin");
 Route::post('/register', [UserController::class, 'register'])->name("register");
 
-<<<<<<< HEAD
+// <<<<<<< HEAD
 Route::get('/profile/{id}', [UserController::class, 'profile'])->name('display-profile')->middleware('checker');
 Route::get('/logout', [UserController::class, 'logout'])->middleware('checker');
 
@@ -43,7 +43,7 @@ Route::get('/seller/new-order', [SellerController::class, 'new_order']);
 Route::get('/seller/old-order', [SellerController::class, 'old_order']);
 Route::get('/seller/profile', [SellerController::class, 'profile']);
 
-=======
+// =======
 Route::prefix('seller')->group(function () {
     Route::get('/dashboard', [SellerController::class, 'dashboard']);
     Route::get('/add-product', [SellerController::class, 'add_product']);
@@ -51,4 +51,4 @@ Route::prefix('seller')->group(function () {
     Route::get('/old-order', [SellerController::class, 'old_order']);
     Route::get('/profile', [SellerController::class, 'profile']);
 });
->>>>>>> ff21cf7874e4a29d97c56cc5188b8216025fa800
+// >>>>>>> ff21cf7874e4a29d97c56cc5188b8216025fa800
