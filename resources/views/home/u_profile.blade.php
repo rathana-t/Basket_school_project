0f0621d19b51c79f2bc7582a8f9fb4cb578343a3
Sivcheu <choeng.sivcheu02@gmail.com>
2021-07-18 20:02:19 +0700

    edit some style on logout

--- resources/views/home/u_profile.blade.php
+++ resources/views/home/u_profile.blade.php
@@ -1,60 +1,38 @@
 @extends('application')
 
 @section('content')
-
-<h3>{{ $data_user->username }}</h3>
-<h3>{{ $data_user->phone }}</h3>
-
-<button class="btn btn-danger" onclick="document.getElementById('id01').style.display='block'">Log Out</button>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-<div id="id01" class="modal">
-    <form class="modal-content" action="/action_page.php">
-      <div class="con">
-        <h1>Delete this</h1>
-        <p>Are you sure! you want to logOut?</p>
-        <div class="clearfix">
-          <button type="button" onclick="document.getElementById('id01').style.display='none'" class="cancelbtn tmenh">Cancel</button>
-                                      {{-- <button type="submit" class="btn btn-danger">Delete</button> --}}
-          <button type="submit" onclick="document.getElementById('id01').style.display='none'" class="deletebtn tmenh"><a href="{{ url('logout') }}">LogOut</a></button>
+    <div class="container text-center">
+        <div class="mt-5">
+            <h3>{{ $data_user->username }}</h3>
+            <h3>{{ $data_user->phone }}</h3>
+            <button class="btn btn-danger" onclick="document.getElementById('id01').style.display='block'">Log Out</button>
         </div>
-      </div>
-    </form>
-  </div>
 
+        <div id="id01" class="modal">
+            <div class="container">
+                <div class="alert alert-warning" role="alert">
+                    <p>Are you sure! you want to logout?</p>
+                    <div class="d-flex justify-content-center">
+                        <button type="button" onclick="document.getElementById('id01').style.display='none'"
+                            class="btn btn-secondary">Cancel</button>
+                        <form action="/logout" method="GET">
+                            <button type="submit" onclick="document.getElementById('id01').style.display='none'"
+                                class="btn btn-danger">LogOut</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
 
-<script>
-    // Get the modal
-    var modal = document.getElementById('id01');
-    
-    // When the user clicks anywhere outside of the modal, close it
-    window.onclick = function(event) {
-      if (event.target == modal) {
-        modal.style.display = "none";
-      }
-    }
+    </div>
+
+    <script>
+        var modal = document.getElementById('id01');
+        window.onclick = function(event) {
+            if (event.target == modal) {
+                modal.style.display = "none";
+            }
+        }
     </script>
 
-@endsection {{-- @stop  --}}
-
+@endsection
