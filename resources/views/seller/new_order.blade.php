422292f134c1d19d00e2151c06d8e8af1dd20b6f
Theanvitou Try <theanvitoutry@gmail.com>
2021-07-31 13:22:59 +0700

    old-olderPage(Frontend) and change some style in new-olderPage

--- resources/views/seller/new_order.blade.php
+++ resources/views/seller/new_order.blade.php
@@ -1,59 +1,45 @@
 @extends('seller\seller')
 
 @section('sidebar-content')
-
-    <style>
-        .out-table {
-            background-color: #EDEBFA;
-            padding: 50px
-        }
-
-        .out-table .set tr .white-box {
-            background-color: white;
-            padding: 20px;
-        }
-
-    </style>
-    <div class="text-center">
-        <h1>
-            This is new order page
-        </h1>
-    </div>
-    <div class="out-table">
-        <table class="table text-center">
-            <thead>
-                <div>
-                    <tr>
-                        <th>No.</th>
-                        <th class="">Customer Name</th>
-                        <th>Product Name</th>
-                        <th>Color</th>
-                        <th>Price</th>
-                        <th>Quality</th>
-                        <th>Total Price</th>
-                        <th>OrderDate</th>
-                        <th>Action</th>
-                    </tr>
-                </div>
-            </thead>
-            <tbody class="set">
-                <tr>
-                    <div class="white-box">
-                        <td>#</td>
-                        <td>#</td>
-                        <td>#</td>
-                        <td>#</td>
-                        <td>#</td>
-                        <td>#</td>
-                        <td>#</td>
-                        <td>#</td>
-                        <td>
-                            <a href="#"><img src="/images/logo/EditButtonSeller.svg"></a>
-                            <a href="#"><img src="/images/logo/DeleteButton.svg"></a>
-                        </td>
-                    </div>
-                </tr>
-            </tbody>
-        </table>
-    </div>
-@stop
+    <table class="table table-hover">
+        <thead>
+            <tr class="text-center">
+                <th scope="col">Image</th>
+                <th scope="col">NO.</th>
+                <th scope="col">Name</th>
+                <th scope="col">Address</th>
+                <th scope="col">Product</th>
+                <th scope="col">Price</th>
+                <th scope="col">Quality</th>
+                <th scope="col">Total price</th>
+                <th scope="col">OrderDate</th>
+                <th scope="col">Action</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr class="seller-list text-center">
+                <td>...</td>
+                <td>...</td>
+                <td>...</td>
+                <td>...</td>
+                <td>...</td>
+                <td>...</td>
+                <td>...</td>
+                <td>...</td>
+                <td>...</td>
+                <td>
+                    <a href="">
+                        <button class="btn btn-primary">
+                            Confirm
+                        </button>
+                    </a>
+                    <a href="">
+                        <button class="btn btn-danger">
+                            Delete
+                        </button>
+                    </a>
+                </td>
+            </tr>
+        </tbody>
+    </table>
+@endsection
--- resources/views/seller/old_order.blade.php
+++ resources/views/seller/old_order.blade.php
@@ -6,4 +6,40 @@
             This is old order page
         </h1>
     </div>
+    <table class="table table-hover">
+        <thead>
+            <tr class="text-center">
+                <th scope="col">Image</th>
+                <th scope="col">NO.</th>
+                <th scope="col">Name</th>
+                <th scope="col">Address</th>
+                <th scope="col">Product</th>
+                <th scope="col">Price</th>
+                <th scope="col">Quality</th>
+                <th scope="col">Total price</th>
+                <th scope="col">OrderDate</th>
+                <th scope="col">Action</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr class="seller-list text-center">
+                <td>...</td>
+                <td>...</td>
+                <td>...</td>
+                <td>...</td>
+                <td>...</td>
+                <td>...</td>
+                <td>...</td>
+                <td>...</td>
+                <td>...</td>
+                <td>
+                    <a href="">
+                        <button class="btn btn-danger">
+                            Delete
+                        </button>
+                    </a>
+                </td>
+            </tr>
+        </tbody>
+    </table>
 @stop
