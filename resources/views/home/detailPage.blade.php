ffc9907970aa9950f963907fd71d12308d9795e0
Sivcheu <choeng.sivcheu02@gmail.com>
2021-08-13 17:07:50 +0700

    style detail page

--- resources/views/home/detailPage.blade.php
+++ resources/views/home/detailPage.blade.php
@@ -3,7 +3,29 @@
 @section('content')
     @include('/home/components/navigation')
     @include('/admin/components/msg')
+    <style>
+        .wrapper {
+            max-height: 400px;
+            display: flex;
+            overflow-x: auto;
+        }
 
+        .wrapper .Item img {
+            height: 100px;
+            object-fit: contain;
+        }
+
+        .wrapper::-webkit-scrollbar {
+            height: 5px;
+            width: 10px;
+        }
+
+        ::-webkit-scrollbar-thumb {
+            background: #afb3b6;
+            border-radius: 5px;
+        }
+
+    </style>
     <div class="container">
         @foreach ($detail_pro as $detail)
             <div class="deatail_page mt-5">
@@ -13,14 +35,18 @@
                             <?php foreach (json_decode($detail->img_product)as $picture) { ?>
                             <img src="{{ asset('images/imgProduct') }}/{{ $picture }}" alt="" class="img-fluid p-4">
                             <?php break; } ?>
-                            <div class="row">
-                                <div class="pl-4 pr-4">
-                                    <div class="col-md-3">
-                                        <?php foreach (json_decode($detail->img_product)as $picture) { ?>
-                                        <img src="{{ asset('images/imgProduct') }}/{{ $picture }}" alt=""
-                                            class="img-fluid pb-4">
-                                        <?php } ?>
+                            <div class="p-2">
+                                <div class="wrapper">
+                                    <?php foreach (json_decode($detail->img_product)as $picture) { ?>
+                                    <div class="col-md-4">
+                                        <div class="Item mb-2 card">
+                                            <div class="p-2">
+                                                <img src="{{ asset('images/imgProduct') }}/{{ $picture }}" alt=""
+                                                    class="img-fluid">
+                                            </div>
+                                        </div>
                                     </div>
+                                    <?php } ?>
                                 </div>
                             </div>
                         </div>
@@ -54,7 +80,6 @@
                                     <span><strong>Store</strong></span>
                                     <span class="pl-4">Phnom Penh</span>
                                 </p>
-
                                 <p class="store_name">
                                     <span><strong>Brand</strong></span>
                                     <span class="pl-4"> {{ $detail->brand_name }} </span>
@@ -91,26 +116,35 @@
                                                     </span>
                                                 </div>
                                             </div> --}}
-                                            <input type="number" class="form-group col-md-2" value="1" id="quantity"
-                                                placeholder="Qty" required name="quantity" min="1"
-                                                max="{{ $detail->stock }}">
-                                            <button type="submit" class="btn btn-primary">
+                                            <input type="number" class="form-control form-control-sm" value="1"
+                                                id="quantity" placeholder="Qty" required name="quantity" min="1"
+                                                max="{{ $detail->stock }}" style="width: 170px">
+                                            <button type="submit" class="btn btn-primary mt-2" style="width: 170px">
+                                                <span class="p-2">
+                                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
+                                                        fill="currentColor" class="bi bi-bag-check-fill"
+                                                        viewBox="0 0 16 16">
+                                                        <path fill-rule="evenodd"
+                                                            d="M10.5 3.5a2.5 2.5 0 0 0-5 0V4h5v-.5zm1 0V4H15v10a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V4h3.5v-.5a3.5 3.5 0 1 1 7 0zm-.646 5.354a.5.5 0 0 0-.708-.708L7.5 10.793 6.354 9.646a.5.5 0 1 0-.708.708l1.5 1.5a.5.5 0 0 0 .708 0l3-3z" />
+                                                    </svg>
+                                                </span>
                                                 Add to cart
                                             </button>
 
                                         </form>
-                                        <form action="{{ route('add_to_wishlist') }}" method="POST">
+                                        <form action="{{ route('add_to_wishlist') }}" method="POST" class="mt-2">
                                             @csrf
                                             <input type="hidden" name="u_id" value="{{ $data_user->id }}" id="">
-
                                             <input type="hidden" name="pro_id" value="{{ $detail->id }}" id="">
-                                            <button type="submit" class="btn btn-warning">
-                                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
-                                                    fill="currentColor" class="bi bi-heart" viewBox="0 0 16 16">
-                                                    <path
-                                                        d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z" />
-                                                </svg>
-                                                Add to wishlist
+                                            <button type="submit" class="btn btn-danger" style="width: 170px">
+                                                <span class="p-2">
+                                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16"
+                                                        fill="currentColor" class="bi bi-heart" viewBox="0 0 16 16">
+                                                        <path
+                                                            d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z" />
+                                                    </svg>
+                                                </span>
+                                                Wishlist
                                             </button>
                                         </form>
 
