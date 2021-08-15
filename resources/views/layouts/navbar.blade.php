c858f1257e86bbeb6fb06ddb1e9a50005490c46d
Sivcheu <choeng.sivcheu02@gmail.com>
2021-08-15 21:05:34 +0700

    navbar count item

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -24,13 +24,14 @@
             </ul>
 
             <ul class="navbar-nav ml-auto">
-                <li class="nav-item cs-pt">
+                <li class="nav-item custom-pt">
                     <a class="nav-link" href="{{ url('wishlist') }}">
                         <svg xmlns="http://www.w3.org/2000/svg" width="30" height="25" fill="currentColor"
                             class="bi bi-heart" viewBox="0 0 16 16">
                             <path
                                 d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z" />
                         </svg>
+                        <span class="badge badge-primary badge-pill">14</span>
                     </a>
                 </li>
                 <a class="nav-link" href="{{ url('/cart') }}">
@@ -41,6 +42,9 @@
                         <path
                             d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1zm3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4h-3.5zM2 5h12v9a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V5z" />
                     </svg>
+                    <span class="badge badge-primary badge-pill">
+                        {{ $counter }}
+                    </span>
                 </a>
                 @if (Session::has('user'))
                     <li class="nav-item">
