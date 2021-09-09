7e9d7de88c85176e80acdb800a0a4c056fabcab4
Sivcheu <choeng.sivcheu02@gmail.com>
2021-09-09 11:24:54 +0700

    db

--- resources/views/admin/product/commission.blade.php
+++ resources/views/admin/product/commission.blade.php
@@ -4,26 +4,34 @@
     @include('/admin/components/modal')
 
 
-    edit
+
     <form action="{{ url('admin/update/commission') }}" method="POST">
         @csrf
-        <input type="text" name="commission" value="{{ $commission }}" id="">
-        <button type="submit">update</button>
+        <h5>
+            Commission
+        </h5>
+        <div class="form-row">
+            <div class="form-group col-md-4">
+                <input type="text" name="commission" value="{{ $commission }}" id="" class="form-control">
+            </div>
+            <div class="form-group col-md-4">
+                <button type="submit" class="btn btn-dark">Update</button>
+            </div>
+        </div>
     </form>
 
-    <div>
+    <small class="text-muted form-text">
         Commission:
         @if ($commission == '')
             not set yet 0 %
         @else
             {{ $commission }} %
         @endif
-    </div>
-    <br>
+    </small class="text-muted form-text">
     <div>
         <a href="{{ url('admin/export/allcommission') }}">
             <button class="btn btn-primary">
-                Export Excel all sellers
+                Export All Shop
             </button>
         </a>
     </div>
@@ -36,90 +44,76 @@
             ?>
             @foreach ($report as $item)
                 @if ($itemA->id == $item->seller_id)
-                    @if ($index == 0)
-                        <div class="text-center">{{ $itemA->store_name }}</div>
-                        <div class="text-center">
-                            <a href="{{ url('admin/export/single/commission', $itemA->id) }}">
-                                <button class="btn btn-primary">
-                                    Export Excel this seller
-                                </button>
-                            </a>
-                        </div>
+                    <div class="card">
+<div class="card-body"></div>
+                        @if ($index == 0)
+                            <div class="text-center">{{ $itemA->store_name }}</div>
+                            <div class="text-center">
+                                <a href="{{ url('admin/export/single/commission', $itemA->id) }}">
+                                    <button class="btn btn-primary">
+                                        Export Excel this seller
+                                    </button>
+                                </a>
+                            </div>
 
-                        <div class="row">
-                            <div class="col-md-1 text-center">
-                                <strong>
+                            <div class="row">
+                                <div class="col">
                                     No
-                                </strong>
-                            </div>
-                            <div class="col-md-2 text-center">
-                                <strong>
-                                    Code Product
-                                </strong>
-                            </div>
-                            <div class="col-md-2 text-center">
-                                <strong>
+                                </div>
+                                {{-- <div class="col-md-2 text-center">
+                                Code Product
+                            </div> --}}
+                                <div class="col">
                                     Product
-                                </strong>
-                            </div>
-                            <div class="col-md-1 text-center">
-                                <strong>
+                                </div>
+                                <div class="col">
                                     Price
-                                </strong>
-                            </div>
-                            <div class="col-md-1 text-center">
-                                <strong>
+                                </div>
+                                <div class="col">
                                     Qauntity Order
-                                </strong>
-                            </div>
-                            <div class="col-md-1 text-center">
-                                <strong>
+                                </div>
+                                <div class="col">
                                     Total Price
-                                </strong>
-                            </div>
-                            <div class="col-md-2 text-center">
-                                <strong>
+                                </div>
+                                <div class="col">
                                     Commission
-                                </strong>
-                            </div>
-                            <div class="col-md-2 text-center">
-                                <strong>
+                                </div>
+                                <div class="col">
                                     Commission Price
-                                </strong>
+                                </div>
                             </div>
-                        </div>
-                        <hr>
-                        <?php
-                        $index = 1;
-                        ?>
-                    @endif
-                    <div class="row">
-                        <div class="col-md-1 text-center">
-                            {{ $no++ }}
-                        </div>
-                        <div class="col-md-2 text-center">
+                            <?php
+                            $index = 1;
+                            ?>
+                        @endif
+                        <div class="row">
+                            <div class="col">
+                                {{ $no++ }}
+                            </div>
+                            {{-- <div class="col-md-2 text-center">
                             {{ $item->code_product }}
-                        </div>
-                        <div class="col-md-2 text-center">
-                            {{ $item->pro_name }}
-                        </div>
-                        <div class="col-md-1 text-center">
-                            {{ $item->pro_price }}
-                        </div>
-                        <div class="col-md-1 text-center">
-                            {{ $item->quantity_order }}
-                        </div>
-                        <div class="col-md-1 text-center">
-                            {{ $item->total_price }}
-                        </div>
-                        <div class="col-md-2 text-center">
-                            {{ $item->commission }}
-                        </div>
-                        <div class="col-md-2 text-center">
-                            {{ $item->commission_price }}
+                        </div> --}}
+                            <div class="col">
+                                {{ $item->pro_name }}
+                            </div>
+                            <div class="col">
+                                {{ $item->pro_price }}
+                            </div>
+                            <div class="col">
+                                {{ $item->quantity_order }}
+                            </div>
+                            <div class="col">
+                                {{ $item->total_price }}
+                            </div>
+                            <div class="col">
+                                {{ $item->commission }}
+                            </div>
+                            <div class="col">
+                                {{ $item->commission_price }}
+                            </div>
                         </div>
                     </div>
-                    <hr>
+
                 @endif
             @endforeach
         </div>
